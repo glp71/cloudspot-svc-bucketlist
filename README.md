a1833591486426398c2e97031ee09e24362dd4ca
Gian Lucas Poltera <glp@gmx.ch>
2020-10-11 18:03:16 +0200

    Initial Commit Initial code is the result from a RESTful API Tutorial.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cloudspot Service: Bucketlist
+
+This is the main REST service of the cloudspot platform, managing the bucketlist.
---
+++ parameters.js
@@ -0,0 +1,7 @@
+var params = {
+    mongodb: {
+        url: 'mongodb+srv://test:test4now@cluster0.4d1s3.mongodb.net/tutorial?retryWrites=true&w=majority'
+    }
+};
+
+module.exports = params;
